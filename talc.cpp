762dbb9e5587d9ed6aa63b4523dd73b3ea67ec53
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-09-24 20:41:51 +0200

    Implemented binary expressions ~Roughly

--- talc.cpp
+++ talc.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <stdint.h>
 
-int main(int argc, char **argv)
+//8 + 8 => 16
+
+class Expression
 {
-	std::cout << "1 + 3 = " << 4 <<  std::endl;
+	private:
+		std::vector<int64_t> operands;
+		std::string operation;
+	
+	public:
+		Expression(int64_t op1, int64_t op2, std::string&& optr)
+		{
+			operands.push_back(op1);
+			operands.push_back(op2);
+			operation = optr;
+		}
+		void print_t()
+		{
+			for (int64_t i: operands)
+				std::cout << i << '\n';
+			std::cout << operation << std::endl;
+		}
+};
+
+int main()
+{
+	Expression my_expr(3, 4, "+");
+	my_expr.print_t();
 	return 0;
 }
