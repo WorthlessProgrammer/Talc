af06cfc15418c2b7ea3c80a4713758ad6fe1bd87
WorthlessProgrammer <guapitorecokie39@gmail.com>
2022-09-27 19:29:12 +0200

    Tokenizing Expressions ~Roughly

--- talc.cpp
+++ talc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 
 //8 + 8 => 16
@@ -11,6 +12,11 @@ enum Op {
   Sub = '-',
   Mul = '*',
   Div = '/'
+};
+
+enum TokenKind {
+  Operand,
+  Operation
 };
 
 class Expression
@@ -61,7 +67,37 @@ class Expression
     }
 };
 
+bool is_number(const std::string& s)
+{
+    return !s.empty() && std::find_if(s.begin(), 
+            s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
+
+void exprParser(std::string& expr)  
+{
+  std::string temp;
+  //std::vector<std::string> tokens;
+
+  std::stringstream strm_expr(expr);
+
+  while (getline(strm_expr, temp, ' ')) {
+    if (is_number(temp)) {
+      std::cout << temp << '\n';
+    } else if (temp[0] == '+'
+              || temp[0] == '-') {
+      std::cout << temp << '\n';
+    } 
+  }
+
+}
+
 int main()
+{
+  std::string a = "98   +   8   -  56";
+  exprParser(a); 
+}
+
+int main2()
 {
   Expression my_expr(359, 489, '*');
   my_expr.evalExpr();
